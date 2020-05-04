d4cafd694ef8d4bd280a5d499e3b556d17d49cc8
kotoba <fuliang207209@163.com>
2020-05-04 12:36:39 +0800

    '登录与用户模块'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,35 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+// 配置请求根路径
+// 本机地址
+// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 远程后台地址
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+
+// 在request 拦截器中, 展示进度条 NProgress.start()
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config => {
+  // NProgress.start()
+  // console.log(config)
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+// response 拦截器中,  隐藏进度条NProgress.done()
+axios.interceptors.response.use(config => {
+  // NProgress.done()
+  return config
+})
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../views/Login.vue')
+// import Home from './components/Home.vue'
+const Home = () => import(/* webpackChunkName: "login_home_welcome" */ '../views/Home.vue')
+// import Welcome from './components/Welcome.vue'
+const Welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../views/Welcome.vue')
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../views/user/Users.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    //   { path: '/rights', component: Rights },
+    //   { path: '/roles', component: Roles },
+    //   { path: '/categories', component: Cate },
+    //   { path: '/params', component: Params },
+    //   { path: '/goods', component: GoodsList },
+    //   { path: '/goods/add', component: Add },
+    //   { path: '/orders', component: Order },
+    //   { path: '/reports', component: Report }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //     next()  放行    next('/login')  强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
